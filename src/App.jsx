13877a9139cf01d5a56712492cbb078aaf56d244
rashadraz <akyrashad@gmail.com>
2024-08-12 20:34:17 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import { Box, Stack } from "@mui/material";
+import { Box, createTheme, Stack } from "@mui/material";
 import Feed from "./components/Feed";
 import RightBar from "./components/RightBar";
 import SideBar from "./components/SideBar";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "@emotion/react";
+import { useState } from "react";
+import Add from "./components/Add";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <Box>
-      <Stack direction={"row"} spacing={2} justifyContent={"space-evenly"}>
-        <SideBar />
-        <Feed />
-        <RightBar />
-      </Stack>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <SideBar setMode={setMode} mode={mode} />
+          <Feed />
+          <RightBar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
